f85304dc0332c4b3f03f1fa595b0cfa8502e1911
kimdoi <kimdoiary@gmail.com>
2024-11-05 01:03:57 +0900

    Feat: disconnect시 deactive 호출하도록

--- functions/chat-app/handlers/disconnect.js
+++ functions/chat-app/handlers/disconnect.js
@@ -1,25 +1,25 @@
 // handlers/disconnect.js
 // WebSocket 연결이 해제될 때 호출
 // 세션 상태를 isSessionActive = false로 업데이트
+const { markSessionDeactive } = require("../common/ddb/dynamoDbClient");
 
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
 
 module.exports.handler = async (event) => {
   const connectionId = event.requestContext.connectionId;
-  const customerId = event.queryStringParameters.customerId; // 고객 ID를 쿼리 매개변수에서 가져온다고 가정
-
+  const order = event.queryStringParameters.order;
   console.log(`Disconnected - ConnectionId: ${connectionId}`);
 
   try {
-    await deleteConnection(connectionId);
-    console.log(`ConnectionId ${connectionId} successfully deleted disconnect`);
+    await markSessionDeactive(orderId);
+    console.log(`orderId ${orderId} successfully deleted disconnect`);
     return {
       statusCode: 200,
-      body: `Disconnected - ConnectionId: ${connectionId}`,
+      body: `Disconnected - orderId: ${orderId}`,
     };
   } catch (error) {
     console.error(
-      `Error during disconnect process for ConnectionId: ${connectionId}`,
+      `Error during disconnect process for orderId: ${orderId}`,
       error
     );
     return {

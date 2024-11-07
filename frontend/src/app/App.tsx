0a35dba4d71e344ec648d1594a27ec96be246730
박민호 <qkralsgh5508@naver.com>
2024-11-07 21:18:45 +0900

    feat:front 웹소켓 연결, stt 구현

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,7 +1,8 @@
 import styles from './page.module.scss';
 import React from 'react';
-import { ChatUI } from '../features/chat/ui/ChatUI';
-
+// import { Auth } from '../features';
+import { ChatUI } from '../features';
+// import { Voice } from '../features';
 const App: React.FC = () => {
   return (
     <div className={styles.customauthenticator}>

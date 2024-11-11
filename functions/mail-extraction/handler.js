142c79315f75960db24131d2b43ad37c328b4a04
정현조 <j96263732@gmail.com>
2024-11-11 13:42:27 +0900

    Refactor: 도시 코드 추가

--- functions/mail-extraction/handler.js
+++ functions/mail-extraction/handler.js
@@ -6,6 +6,104 @@ const s3 = new AWS.S3();
 const sqs = new AWS.SQS({ region: 'ap-northeast-2' });
 const textract = new AWS.Textract();
 const openAI = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const cityMapping = {
+  // Major cities
+  서울: 'SEL',
+  인천: 'ICN',
+  부산: 'PUS',
+  대구: 'TAE',
+  대전: 'DCC',
+  광주: 'KWJ',
+  울산: 'USN',
+  제주: 'CJU',
+  수원: 'SWU',
+  성남: 'SNM',
+  춘천: 'CHN',
+  강릉: 'GRN',
+
+  // Airports
+  김포공항: 'GMP',
+  인천공항: 'ICN',
+  제주공항: 'CJU',
+  김해공항: 'PUS',
+  대구공항: 'TAE',
+  청주공항: 'CJJ',
+  광주공항: 'KWJ',
+  울산공항: 'USN',
+  여수공항: 'RSU',
+  포항공항: 'KPO',
+  양양공항: 'YNY',
+  원주공항: 'WJU',
+  무안공항: 'MWX',
+  사천공항: 'HIN',
+
+  // Train stations
+  서울역: 'SEO',
+  용산역: 'YON',
+  부산역: 'PSN',
+  대전역: 'DJC',
+  대구역: 'TAE',
+  광주송정역: 'GWJ',
+  울산역: 'USN',
+  인천역: 'INC',
+  춘천역: 'CHC',
+  강릉역: 'GRG',
+  수서역: 'SUS',
+  동대구역: 'DDG',
+  익산역: 'IKS',
+  전주역: 'JJR',
+  목포역: 'MKP',
+  여수엑스포역: 'YSR',
+  포항역: 'POH',
+
+  // Ports
+  부산항: 'BPH',
+  인천항: 'IPH',
+  울산항: 'UPH',
+  평택항: 'PTH',
+  여수항: 'YHP',
+  목포항: 'MHP',
+  포항항: 'PHP',
+  제주항: 'JPH',
+  광양항: 'GPH',
+  삼천포항: 'SCH',
+  동해항: 'DHH',
+  속초항: 'SCH',
+  마산항: 'MSH',
+  진해항: 'JHH',
+  군산항: 'GSH',
+
+  // Additional locations
+  서산항: 'SSH',
+  태안항: 'TAH',
+  울릉항: 'ULH',
+  독도항: 'DDH',
+  화물터미널: 'CFT',
+  국제물류기지: 'ILB',
+  남해항: 'NHN',
+  거제항: 'GJH',
+
+  // International Ports and Airports (for reference)
+  상하이항: 'SHA',
+  홍콩항: 'HKG',
+  싱가포르항: 'SIN',
+  도쿄항: 'TYO',
+  뉴욕항: 'NYH',
+  로스앤젤레스항: 'LAH',
+  런던항: 'LDH',
+  함부르크항: 'HAM',
+  두바이항: 'DXB',
+};
+
+// 필드명과 한국어 매핑
+const fieldMapping = {
+  Weight: '중량',
+  ContainerSize: '컨테이너 크기',
+  DepartureDate: '출발 날짜',
+  ArrivalDate: '도착 날짜',
+  DepartureCity: '출발 도시',
+  ArrivalCity: '도착 도시',
+};
 
 // 프롬프트 템플릿 정의
 const PROMPT_TEMPLATE = `
@@ -18,10 +116,9 @@ const PROMPT_TEMPLATE = `
 6. ArrivalCity: 도착 도시 (아래 도시 목록에서 선택)
 
 ### 도시 목록과 코드:
-- 서울 (SEL), 인천 (ICN), 부산 (PUS), 대구 (TAE), 대전 (DCC), 광주 (KWJ)
-- 울산 (USN), 제주 (CJU), 김포 (GMP), 포항 (KPO), 양양 (YNY), 청주 (CJJ)
-- 원주 (WJU), 군산 (KUV), 여수 (RSU), 사천 (HIN), 목포 (MPK), 진주 (HIN)
-- 속초 (SHO), 강릉 (KAG), 춘천 (CNX), 안동 (AEO), 전주 (CHN), 삼척 (SCK)
+${Object.entries(cityMapping)
+  .map(([key, code]) => `- ${key} (${code})`)
+  .join('\n')}
 
 모든 결과는 JSON 형식으로 반환합니다. 도시 이름은 목록에 없는 경우 "unknown"으로 표시합니다.
 
@@ -55,31 +152,7 @@ const validateExtractedData = (data) => {
   };
 
   const validContainerSizes = [1, 2, 3, 4];
-  const cityCodes = [
-    'SEL',
-    'ICN',
-    'PUS',
-    'TAE',
-    'DCC',
-    'KWJ',
-    'USN',
-    'CJU',
-    'GMP',
-    'KPO',
-    'YNY',
-    'CJJ',
-    'WJU',
-    'KUV',
-    'RSU',
-    'HIN',
-    'MPK',
-    'SHO',
-    'KAG',
-    'CNX',
-    'AEO',
-    'CHN',
-    'SCK',
-  ];
+  const cityCodes = Object.values(cityMapping);
 
   return {
     Weight: typeof data.Weight === 'number' && data.Weight > 0 ? data.Weight : defaultValues.Weight,

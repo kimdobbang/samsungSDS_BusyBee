62dcc5a10045e1cec90f70374bb025fa5b206817
박민호 <qkralsgh5508@naver.com>
2024-11-11 14:14:20 +0900

    feat:websocket connect, stt develop

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,33 +1,34 @@
 import styles from './page.module.scss';
 import React from 'react';
-<<<<<<< HEAD
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { AuthUI } from '../features';
-=======
 
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { ChatUI, Home, DetailMail, MailList, Dashboard } from 'features';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import {
+  ChatUI,
+  Home,
+  DetailMail,
+  MailList,
+  Dashboard,
+  AuthUI,
+} from 'features';
 import BoardLayout from 'features/board/ui/BoardLayout';
 // import { Auth } from '../features';
->>>>>>> eaccedc30dd99865767d8531432a6919f8c1819d
 // import { Voice } from '../features';
 
 const App: React.FC = () => {
   return (
     <Router>
-<<<<<<< HEAD
-      <Routes>
-        <Route path='/' element={<AuthUI />} />
-        <Route path='/ChatUI' element={<AuthUI />} />
-      </Routes>
-=======
       <div className={styles.customauthenticator}>
         <Routes>
           {/* 홈 페이지 */}
           <Route path='/' element={<Home />} />
 
           {/* 채팅 페이지 */}
-          <Route path='/chatUI' element={<ChatUI />} />
+          <Route path='/ChatUI' element={<AuthUI />} />
 
           {/* 게시판 관련 라우트 */}
           <Route path='/board' element={<BoardLayout />}>
@@ -46,7 +47,6 @@ const App: React.FC = () => {
           </Route>
         </Routes>
       </div>
->>>>>>> eaccedc30dd99865767d8531432a6919f8c1819d
     </Router>
   );
 };

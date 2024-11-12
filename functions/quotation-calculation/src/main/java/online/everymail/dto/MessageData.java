e61776f0370d462b85b646f2025cd49df5be1b37
youngho <youngho.dev@gmail.com>
2024-11-12 13:57:19 +0900

    Feat: DTO 수정

--- functions/quotation-calculation/src/main/java/online/everymail/dto/MessageData.java
+++ functions/quotation-calculation/src/main/java/online/everymail/dto/MessageData.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 public class MessageData {
     private String key;
     private String sender;
+    private String receiver;
+    private String receivedDate;
     private Data data;
     private int status;
     private int quote;
@@ -13,6 +15,8 @@ public class MessageData {
     public MessageData(SQSMessageData sqsMessageData, int quote) {
         this.key = sqsMessageData.getKey();
         this.sender = sqsMessageData.getSender();
+        this.receiver = sqsMessageData.getReceiver();
+        this.receivedDate = sqsMessageData.getReceivedDate();
         this.data = sqsMessageData.getData();
         this.status = sqsMessageData.getStatus();
         this.quote = quote;

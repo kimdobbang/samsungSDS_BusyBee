9b4462e2b5107ee574d0b2da6207f12295741679
박민호 <qkralsgh5508@naver.com>
2024-11-13 13:53:41 +0900

    feat-dashboard-api-develop

--- frontend/src/features/board/utils/estimate.ts
+++ frontend/src/features/board/utils/estimate.ts
@@ -1,3 +1,42 @@
-export const CountInProgressQuotes = (data: any) => {
-  return data.filter((item: any) => item.status.N !== 5).length;
+import { ParseDate } from 'shared/utils/getChangeDate';
+
+export const getTodayOrderMail = (data: any) => {
+  const today = new Date();
+
+  let todayMail: any[] = [];
+
+  data.forEach((item: any) => {
+    const receivedDate = ParseDate(item.received_date.S); // 날짜 파싱
+    if (!receivedDate) return;
+
+    if (
+      receivedDate.getDate() === today.getDate() &&
+      receivedDate.getMonth() === today.getMonth() &&
+      receivedDate.getFullYear() === today.getFullYear()
+    ) {
+      todayMail.push(item);
+    }
+  });
+  todayMail.reverse();
+  return todayMail;
+};
+
+export const getMonthOrderMail = (data: any) => {
+  const today = new Date();
+
+  let MonthMail: any[] = [];
+
+  data.forEach((item: any) => {
+    const receivedDate = ParseDate(item.received_date.S); // 날짜 파싱
+    if (!receivedDate) return;
+
+    if (
+      receivedDate.getMonth() === today.getMonth() &&
+      receivedDate.getFullYear() === today.getFullYear()
+    ) {
+      MonthMail.push(item);
+    }
+  });
+  // MonthMail.reverse();
+  return MonthMail;
 };

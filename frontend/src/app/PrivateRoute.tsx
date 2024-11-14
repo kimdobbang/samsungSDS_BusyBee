387d31f8076b023691ebc595bb0d88cc6f9f501a
Seunghee Goh <86093028+alex-koko@users.noreply.github.com>
2024-11-15 00:26:13 +0900

    Feat: 사용자 그룹에 따라 라우팅 막음

--- frontend/src/app/PrivateRoute.tsx
+++ frontend/src/app/PrivateRoute.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from 'features/auth/hooks/useAuth'; // useAuth 훅의 경로는 프로젝트 구조에 맞게 수정하세요.
 
 const PrivateRoute: React.FC<{ children: JSX.Element }> = ({ children }) => {
   const [group] = useAuth() || [];
-
-  // 사용자가 인증되지 않았을 경우 AuthUI로 리디렉션
+  const location = useLocation();
+  // 사용자가 인증되지 않았을 경우 / 경로로 리디렉션
   if (!group) {
-    return <Navigate to='/authUI' />;
+    return <Navigate to='/' />;
   }
 
+  // 사용자가 인증되지 않았을 경우 / 경로로 리디렉션
+  if (group.length === 0) {
+    return <Navigate to='/' />;
+  }
+
+  // group에 'Admin'이 포함되어 있지 않으면서 현재 경로가 /mail 또는 /mail/detail인 경우 /로 리디렉션
+  if (
+    !group.includes('Admin') &&
+    (location.pathname === '/mail' || location.pathname === '/mail/detail')
+  ) {
+    return <Navigate to='/' />;
+  }
   return children;
 };
 

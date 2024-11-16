47e93dea60bb7945796cb9c3f955b414831697e9
정현조 <j96263732@gmail.com>
2024-11-16 23:54:08 +0900

    Feat: 다이나노db 스트림 -> 람다(필터링) -> online학습 중 필터링 부분 구현

---
+++ functions/maildb-to-sqs/handler.js
@@ -0,0 +1,38 @@
+import { unmarshall } from '@aws-sdk/client-dynamodb';
+import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
+
+const REGION = 'ap-northeast-2';
+const SQS_QUEUE_URL = process.env.SQS_QUEUE_URL;
+
+const sqsClient = new SQSClient({ region: REGION });
+
+export const dynamodbToSQSHandler = async (event) => {
+  try {
+    for (const record of event.Records) {
+      if (record.eventName === 'MODIFY') {
+        const oldImage = unmarshall(record.dynamodb.OldImage);
+        const newImage = unmarshall(record.dynamodb.NewImage);
+
+        if (!oldImage.isTagCollect && newImage.isTagCollect) {
+          const messageBody = {
+            emailId: newImage.emailId,
+            emailContent: newImage.emailContent,
+            flag: newImage.flag,
+          };
+
+          const command = new SendMessageCommand({
+            QueueUrl: SQS_QUEUE_URL,
+            MessageBody: JSON.stringify(messageBody),
+          });
+
+          await sqsClient.send(command);
+          console.log(`Message sent to SQS: ${JSON.stringify(messageBody)}`);
+        }
+      }
+    }
+    return { statusCode: 200, body: 'Successfully processed events.' };
+  } catch (error) {
+    console.error(`Error processing DynamoDB stream: ${error.message}`);
+    throw error;
+  }
+};
